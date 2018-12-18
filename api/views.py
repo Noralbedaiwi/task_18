1c70c7d47a4082d89ff5abb5df20dad2b9b9f638
Nora <n.albedaiwi@hotmail.com>
2018-12-18 20:46:24 +0300

    submission

--- api/views.py
+++ api/views.py
@@ -13,11 +13,15 @@ from .serializers import (
 )
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from .permissions import IsOwner
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
 class RestaurantListView(ListAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantListSerializer
     permission_classes = [AllowAny,]
+    filter_backends = [SearchFilter,OrderingFilter]
+    search_fields = ['title', 'content',]
 
 
 class RestaurantDetailView(RetrieveAPIView):
